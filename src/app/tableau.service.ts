a24f0f92c7d3099ad8e159d1cef346904ab5dbdc
Aveek Mukherjee <aveekdeveloper@gmail.com>
2018-10-30 17:04:07 +0000

    trying to fix calculation showing up in formulas

--- src/app/tableau.service.ts
+++ src/app/tableau.service.ts
@@ -62,13 +62,40 @@ export class TableauService {
   }
 
   handleRecipe(recipe){
-    var expression = jsonata(recipe);
+    var expression = jsonata(recipe.recipe);
     //console.log(expression.evaluate(this.tabFile));
-    return expression.evaluate(this.tabFile);
+    var data = expression.evaluate(this.tabFile);
+
+    //Handle additional treatments if needed
+    switch(recipe.name){
+        case 'List all Formulas':
+            //need to replace calculation_xxxxxxxxx fields with the captions
+            data = this.replaceCalculationsinFormula(data);
+    }
+
+    return data;
   }
 
   //Expand a formula by replacing Calculation_*** with the column caption
-  expandFormula(formula){
+  replaceCalculationsinFormula(data){
+      for(var i=0;i<data.length;i++){
+          var row = data[i];
+          if (row['colid'].includes('Calculation_')){
+              //data[i]['colformula'].replace(row['colid'],'balbla')
+              /*
+              for(var j=0;j<data.length;j++){
+                  //replace the colcaption in all formula columns
+                  data[j]['colformula'].replace(row['colid'],row['colcaption'])
+            }*/
 
+          }else{
+              continue;
+          }
+
+      }
+      return data;
   }
+
+
 }
+
